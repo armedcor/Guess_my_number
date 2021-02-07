23cf288d32b21b10fd6f0a65ac0a50f1096f92b0
Cormac Lawlor <armedcor@gmail.com>
2021-02-07 23:31:07 +0000

    DRY refactor

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   //When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
-
+    displayMessage('⛔ No number!');
     //When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -28,12 +31,11 @@ document.querySelector('.check').addEventListener('click', function () {
     //When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? '📈 Too high!' : '📉 Too Low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too Low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -43,7 +45,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
 
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
