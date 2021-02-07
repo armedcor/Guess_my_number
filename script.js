15c825828f4457438747a851127be451e261012a
Cormac Lawlor <armedcor@gmail.com>
2021-02-07 22:45:20 +0000

    Implement reload button

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -15,6 +14,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
@@ -41,4 +41,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
 });
